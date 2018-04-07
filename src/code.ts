6589eec7b2e1bbdddba2751a163673e95d48418c
Nesters <mail@nesters.me>
2018-04-07 19:30:58 +0300

    Basic Observable emit -> subscribe logic

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,27 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+// Add an item to the DOM list
+const addItem = (val:any) => {
+  const node = document.createElement("li");
+  const textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+};
+
+// Create our Observable 
+const observable = Observable.create(
+  (observer:any) => {
+    observer.next('Hello');
+    observer.next('World');
+    observer.complete();
+    observer.next('!');
+  },
+);
+ 
+// Subscribe to our Observable
+observable.subscribe(
+  (x:any) => addItem(x),
+  (error:any) => addItem(error),
+  () => addItem('Completed'),
+  (nothing:any) => addItem(nothing),
+);
